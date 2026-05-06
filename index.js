41a3ba1b58d0c7312437dffb16f0b60b2243682f
Noelia Rodeiro <nmrodeiro@gmail.com>
2026-05-06 20:50:22 -0300

    Modulo obras sociales completo

--- index.js
+++ index.js
@@ -3,12 +3,14 @@ console.log("clinica");
 import express from "express";
 import  { pool } from "./controlador/conexion.js";
 import { testConexion } from "./controlador/testconexion.js"; 
+import obrasRoutes from "./routes/obrasSociales.routes.js";
 
 const app = express();
 
 await testConexion();
 
 app.use(express.json());
+app.use("/obras-sociales", obrasRoutes);
 
 app.get("/", (req, res) => {
     console.log("test get");
